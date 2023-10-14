1b386d55b9c029e0d8f0a2f3b8657bed19594737
Rian Corcino <riancorci@gmail.com>
2023-10-14 16:07:34 -0700

    updated dependencies

--- app/src/main/java/com/example/marvelapi/MainActivity.kt
+++ app/src/main/java/com/example/marvelapi/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.marvelapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Example API request with authentication
+        val timestamp = System.currentTimeMillis()
+        val privateKey = "1234567890987654321"
+        val publicKey = "1234567890"
+        val hash = stringToMd5("$timestamp$privateKey$publicKey")
+        var client = AsyncHttpClient()
 
+        val url =
+            "https://gateway.marvel.com/v1/public/characters?ts=$timestamp&apikey=$publicKey&hash=$hash"
+        client[url, object : JsonHttpResponseHandler() {
+            // Request results
+        }]
     }
 }
